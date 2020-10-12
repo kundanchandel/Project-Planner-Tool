393b12d16ff2a4324d87401b12d69334408de910
JigyasaAgrawal <jigyasaagrawal28@gmail.com>
2020-10-12 15:29:00 +0530

    updated user schema

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    pastorders: [{
+        type:mongoose.Schema.Types.ObjectId,
+        ref :"Order"
+    }],
+    currentorder: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref: "Order"
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
