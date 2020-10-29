3dad4581af9cfd046da0d684049a6bd959dda996
kundanchandel <kundanchandel08@gmail.com>
2020-10-29 20:53:38 +0530

    updated code

--- server/Routes/user.js
+++ server/Routes/user.js
@@ -7,9 +7,10 @@ const Order = require("../models/Order");
 const Restaurant = require("../models/Restaurant");
 const User = require("../models/User");
 const Dish = require("../models/Dish");
+const mail = require("../utils/mail");
 
 const TOKENSECRET = "superSecretTokenOfQDineIn";
-require('dotenv').config();
+require("dotenv").config();
 
 Router.post("/signup", async (req, res, next) => {
   const { username, email, phoneno, password } = req.body;
@@ -65,23 +66,63 @@ Router.post("/login", async (req, res, next) => {
   );
   res.status(200).json({
     message: "Signed in successfully",
-    token
+    token,
   });
 });
 
-Router.post('/resetpassword',isloggedin,async(req,res,next)=>{
-  const {oldPassword, newPassword} = req.body;
+Router.post("/resetpassword", isloggedin, async (req, res, next) => {
+  const { oldPassword, newPassword } = req.body;
   const userEmail = req.user.userEmail;
-  const user = await User.findOne({email:userEmail});
-  const validpass = await bcrypt.compare(oldPassword, user.password)
-  if(validpass){
+  const user = await User.findOne({ email: userEmail });
+  const validpass = await bcrypt.compare(oldPassword, user.password);
+  if (validpass) {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(newPassword, salt);
     user.password = hashedPassword;
-    user.save()
-    res.status(200).json(user)
-  }else{
-    res.status(200).json({err:"Incorrect old password"})
+    user.save();
+    res.status(200).json(user);
+  } else {
+    res.status(200).json({ err: "Incorrect old password" });
+  }
+});
+
+Router.post("/forgotpassword", async (req, res, next) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    var pass = "";
+    var str =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789@#$";
+    for (i = 1; i <= 8; i++) {
+      var char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(pass, salt);
+    user.password = hashedPassword;
+    user.save();
+    mail(
+      email,
+      "Reset Password Link",
+      `
+  <p>Hello ${user.username}, 
+  <br/><br/>
+  We heard that you lost your password. Sorry about that! 
+  <br/><br/>
+  But don’t worry! You can use the following password to login and reset your password.
+  <br/><br/>
+  Password : ${pass}
+  <br/><br/>
+  If you did not request this, please ignore this email and your password will be ${pass}.
+  <br/><br/>
+  Thanks, <br/>
+  QDine-In Team 
+  </p>
+  `
+    );
+    res.status(200).json({ msg: "Reset Password mail has been sent.." });
+  } else {
+    res.status(200).json({ err: "Invalid Email Address" });
   }
 });
 
@@ -131,26 +172,26 @@ Router.post("/restaurant/:id/order", isloggedin, async (req, res, next) => {
   });
   if (user.currentorder == null) {
     const data = req.body;
-    var tempDish = []
+    var tempDish = [];
     var orderTotal = 0;
     for (var i = 0; i < data.dish.length; i++) {
-	let dishid = data.dish[i]._id;
-	const orderDishes = await Dish.findOne({
-	  _id: dishid,
-	});
-	let total = orderDishes.price * data.dish[i].quantity;
-	orderTotal = orderTotal + total;
-	let tempData = {
-	  orderDishes,
-	  qnt: data.dish[i].quantity,
-	  total,
-	};
-	tempDish.push(tempData);
-      }
+      let dishid = data.dish[i]._id;
+      const orderDishes = await Dish.findOne({
+        _id: dishid,
+      });
+      let total = orderDishes.price * data.dish[i].quantity;
+      orderTotal = orderTotal + total;
+      let tempData = {
+        orderDishes,
+        qnt: data.dish[i].quantity,
+        total,
+      };
+      tempDish.push(tempData);
+    }
     const order = await Order.create({
       dish: tempDish,
       user: user._id,
-      orderTotal:orderTotal
+      orderTotal: orderTotal,
     });
     user.currentorder = order._id;
     user.save();
